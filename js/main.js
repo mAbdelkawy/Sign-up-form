8f56958c69d9b57f077b491a6afbe71c4239f19f
yokova <yokova01@gmail.com>
2022-09-29 17:34:35 +0200

    Prevent form from submitting when passwords does not match

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+let password = document.querySelector("#password");
+let confirm = document.querySelector("#confirm-password");
+let message = document.querySelector("#message");
+
 function onChange() {
-    let password = document.querySelector("#password");
-    let confirm = document.querySelector("#confirm-password");
-    let message = document.querySelector("#message");
     if (confirm.value === password.value) {
         confirm.style.outline = "none";
         confirm.style.border = "2px solid rgb(0, 128, 0,0.7)";
@@ -12,5 +13,13 @@ function onChange() {
         confirm.style.border = "2px solid rgb(255, 0, 0,0.6)";
         message.textContent = "passwords do not match";
         message.style.color = "rgb(255, 0, 0,0.6)";
+    }
+  }
+  function validateMyForm(){
+    if (confirm.value === password.value){
+        return true;
+    }else {
+        returnToPreviousPage();
+        return false;
     }
   }
