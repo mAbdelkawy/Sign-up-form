9f3a0c91b6749d91254f49f47e2fd5efecd5be95
yokova <yokova01@gmail.com>
2022-09-29 17:19:23 +0200

    Validate passwords

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function onChange() {
+    let password = document.querySelector("#password");
+    let confirm = document.querySelector("#confirm-password");
+    let message = document.querySelector("#message");
+    if (confirm.value === password.value) {
+        confirm.style.outline = "none";
+        confirm.style.border = "2px solid rgb(0, 128, 0,0.7)";
+        message.textContent = "passwords match";
+        message.style.color = "rgb(0, 128, 0,0.7)";
+    } else {
+        confirm.style.outline = "none";
+        confirm.style.border = "2px solid rgb(255, 0, 0,0.6)";
+        message.textContent = "passwords do not match";
+        message.style.color = "rgb(255, 0, 0,0.6)";
+    }
+  }
